e912cde67475cae345bef230c9a6f083fc0a2103
DC <dillonchaffey@gmail.com>
2020-01-29 22:35:26 -0400

    1st

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+test_data_array = []
+
+@app.route('/')
+def index():
+	return render_template('index.html', lat=45, lng=65)
+
+'''
+@app.route('/api/<lat>/<lng>) 
+def coordinates():
+  addresses = session.query(Coordinates)#however you query your db
+  all_coods = [] # initialize a list to store your addresses
+  for add in addresses:
+     address_details = {
+     "lat": add.lat, 
+     "lng": add.lng, 
+     "title": add.title}
+     all_coods.append(address_details)
+  return jsonify({'cordinates': all_coods})
+'''
+
+@app.route('/<lat>/<lng>') 
+def display_map(lat, lng):
+	return render_template('index.html', lat=lat,lng=lng)
+
+if __name__ == '__main__':
+	app.run('0.0.0.0',8080)
