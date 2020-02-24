3a0f0686121d0e8d962ac7335f6bea7943466daf
DC <dillonchaffey@gmail.com>
2020-02-23 21:56:35 -0400

    i

--- main.py
+++ main.py
@@ -47,10 +47,9 @@ def get_nearby_wikipedia(lat, lng):
 
 @app.route('/api/buses')
 def get_buses():
-    # response = requests.get('https://kbus.doublemap.com/map/v2/buses');
-    # buses = json.loads(response.text)
-    buses = json.loads(
-        '[{"id":11,"name":"59","lat":45.0709,"lon":-64.54762,"heading":328,"route":66,"lastStop":1748,"fields":{},"bus_type":"bus","lastUpdate":1582415938}]')
+    response = requests.get('https://kbus.doublemap.com/map/v2/buses');
+    buses = json.loads(response.text)
+    # buses = json.loads('[{"id":11,"name":"59","lat":45.0709,"lon":-64.54762,"heading":328,"route":66,"lastStop":1748,"fields":{},"bus_type":"bus","lastUpdate":1582415938}]')
 
     outBuses = []
     for b in buses:
